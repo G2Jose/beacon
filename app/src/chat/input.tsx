6f4ca719fee1fd2e95ba4fec086363b479157f5f
George Jose <george.jose.a@gmail.com>
2019-01-20 23:40:27 -0500

    Button

--- app/src/chat/input.tsx
+++ app/src/chat/input.tsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
-import { TextInput, StyleSheet, ViewStyle, StyleProp } from 'react-native'
+import {
+  TextInput,
+  StyleSheet,
+  ViewStyle,
+  StyleProp,
+  View,
+  Button,
+  TouchableOpacity,
+  Text,
+} from 'react-native'
 
 interface Props {
   style?: StyleProp<ViewStyle>
@@ -11,15 +20,35 @@ export default class MessageInput extends Component<Props> {
   render() {
     const { style } = this.props
     return (
-      <TextInput
-        style={[styles.input, style]}
-        onChangeText={text => this.setState({ text })}
-        value={this.state.text}
-      />
+      <View style={[styles.container, style]}>
+        <TextInput
+          style={styles.input}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.icon}>⬆</Text>
+        </TouchableOpacity>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  input: { height: 40, borderColor: 'gray', borderWidth: 1 },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 7,
+    flex: 1,
+    marginRight: 7,
+  },
+  icon: { fontSize: 35 },
 })
